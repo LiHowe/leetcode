55b48456d4de0d887b852327f73a4d28ffb2afb9
Howe <howe.personal@gmail.com>
2021-04-27 16:10:46 +0800

    daily exercises (DP)

---
+++ utils.js
@@ -0,0 +1,29 @@
+const timeLog = (name, fn, args, expect) => {
+    console.time(name)
+    const output = fn(...args)
+    console.log('output ->', output)
+    console.log('result ->', output === expect ? '✅' : '❌')
+    console.timeEnd(name)
+}
+const separator = () => {
+    console.log('-----------------------------')
+}
+
+module.exports = {
+    /**
+     * 
+     * @param {Function} fn 
+     * @param {Array} args 
+     * @param {any} expect 
+     */
+    test: (fn, args, expect) => {
+        if (!(args instanceof Array)) {
+            args = [args]
+        }
+        separator()
+        console.log('input ->', args)
+        console.log('expect ->', expect)
+        timeLog('runtime', fn, args, expect)
+        separator()
+    },
+}
