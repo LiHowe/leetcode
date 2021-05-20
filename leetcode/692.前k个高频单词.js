51da722dcdf4cc32cab7573681aabbfcf5eb5167
Howe <howe.personal@gmail.com>
2021-05-20 16:21:57 +0800

    checkin

---
+++ leetcode/692.前k个高频单词.js
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=692 lang=javascript
+ *
+ * [692] 前K个高频单词
+ *
+ * https://leetcode-cn.com/problems/top-k-frequent-words/description/
+ *
+ * algorithms
+ * Medium (52.59%)
+ * Likes:    262
+ * Dislikes: 0
+ * Total Accepted:    33.5K
+ * Total Submissions: 62.3K
+ * Testcase Example:  '["i", "love", "leetcode", "i", "love", "coding"]\n2'
+ *
+ * 给一非空的单词列表，返回前 k 个出现次数最多的单词。
+ * 
+ * 返回的答案应该按单词出现频率由高到低排序。如果不同的单词有相同出现频率，按字母顺序排序。
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ * 输出: ["i", "love"]
+ * 解析: "i" 和 "love" 为出现次数最多的两个单词，均为2次。
+ * ⁠   注意，按字母顺序 "i" 在 "love" 之前。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
+ * k = 4
+ * 输出: ["the", "is", "sunny", "day"]
+ * 解析: "the", "is", "sunny" 和 "day" 是出现次数最多的四个单词，
+ * ⁠   出现次数依次为 4, 3, 2 和 1 次。
+ * 
+ * 
+ * 
+ * 
+ * 注意：
+ * 
+ * 
+ * 假定 k 总为有效值， 1 ≤ k ≤ 集合元素数。
+ * 输入的单词均由小写字母组成。
+ * 
+ * 
+ * 
+ * 
+ * 扩展练习：
+ * 
+ * 
+ * 尝试以 O(n log k) 时间复杂度和 O(n) 空间复杂度解决。
+ * 
+ * 
+ */
+
+const { test } = require('./utils')
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    return Object.values(words.reduce((res, w) => {
+        if (!res[w]) {
+            res[w] = {
+                w,
+                n: 0
+            }
+        }
+        res[w].n++
+        return res
+    }, {}))
+    // .sort((a, b) => b.n - a.n || a.w.charCodeAt(0) - b.w.charCodeAt(0))
+    .sort((a, b) => {
+        const res = b.n - a.n
+        return res ? res : a.w > b.w ? 1 : -1
+    })
+    .slice(0, k)
+    .map(item => item.w)
+};
+// @lc code=end
+
+test(topKFrequent, [["love", "i", "leetcode", "i", "love", "coding"], 2], ["i", "love"])
+test(topKFrequent, [["i", "love", "leetcode", "i", "love", "coding"], 3], ["i","love","coding"])
+test(topKFrequent, [["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], 4], ["the", "is", "sunny", "day"])
+test(topKFrequent, [["aaa","aa","a"], 1], ["a"]) // ????? why ["a"]
+test(topKFrequent, [["plpaboutit","jnoqzdute","sfvkdqf","mjc","nkpllqzjzp","foqqenbey","ssnanizsav","nkpllqzjzp","sfvkdqf","isnjmy","pnqsz","hhqpvvt","fvvdtpnzx","jkqonvenhx","cyxwlef","hhqpvvt","fvvdtpnzx","plpaboutit","sfvkdqf","mjc","fvvdtpnzx","bwumsj","foqqenbey","isnjmy","nkpllqzjzp","hhqpvvt","foqqenbey","fvvdtpnzx","bwumsj","hhqpvvt","fvvdtpnzx","jkqonvenhx","jnoqzdute","foqqenbey","jnoqzdute","foqqenbey","hhqpvvt","ssnanizsav","mjc","foqqenbey","bwumsj","ssnanizsav","fvvdtpnzx","nkpllqzjzp","jkqonvenhx","hhqpvvt","mjc","isnjmy","bwumsj","pnqsz","hhqpvvt","nkpllqzjzp","jnoqzdute","pnqsz","nkpllqzjzp","jnoqzdute","foqqenbey","nkpllqzjzp","hhqpvvt","fvvdtpnzx","plpaboutit","jnoqzdute","sfvkdqf","fvvdtpnzx","jkqonvenhx","jnoqzdute","nkpllqzjzp","jnoqzdute","fvvdtpnzx","jkqonvenhx","hhqpvvt","isnjmy","jkqonvenhx","ssnanizsav","jnoqzdute","jkqonvenhx","fvvdtpnzx","hhqpvvt","bwumsj","nkpllqzjzp","bwumsj","jkqonvenhx","jnoqzdute","pnqsz","foqqenbey","sfvkdqf","sfvkdqf"], 1], ["fvvdtpnzx"])
